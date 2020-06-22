a9026b397c4c624ea0c7d62c57b5976b03c246d0
florian <duceptflorian53@gmail.com>
2020-06-22 11:52:59 +0200

    deuxieme commit

--- index.js
+++ index.js
@@ -14,16 +14,42 @@ document.querySelector("#button").addEventListener("click", (e) => {
   e.preventDefault();
   send()
 })*/
+const isValid = (value) => {
+  let reg = /[0-9]{5}/;
+  return (value.length = 5 && reg.test(value));
+};
+function complete(obj) {
+  for (let i = 0; i < obj.length; i++) {
+    let neo = document.createElement("p");
+    result.appendChild(neo);
+    neo.innerHTML = `Ville : ${obj[i].nom}`;
+    neo.style.color = "black";
+  }
+}
 function get(postcode) {
   let request = new XMLHttpRequest();
-  request.open("GET", `http://api.zippopotam.us/fr/${postcode}`);
+  request.open(
+    "GET",
+    `https://geo.api.gouv.fr/communes?codePostal=${postcode}`
+  );
   request.onreadystatechange = function () {
     if ((this.readyState = XMLHttpRequest.DONE && this.status == 200)) {
-      let response = JSON.parse(this.responseText);
-      console.log(response.country);
-      return response;
+      const response = JSON.parse(this.responseText);
+      complete(response);
+      if (response.length === 0) {
+        result.innerHTML = "Cette adresse postale n'existe pas";
+      }
     }
   };
   request.send();
 }
-document.querySelector("#");
+let result = document.querySelector(".main__result");
+document.querySelector("#sub-btn").addEventListener("click", (e) => {
+  let sode = document.querySelector('input[type="text"]').value;
+  e.preventDefault();
+  if (isValid(sode)) {
+    get(sode);
+  } else {
+    result.innerHTML = "Entrer une addresse valide";
+  }
+});
