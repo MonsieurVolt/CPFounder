5c50725347ff1d9f46d1c3d459a80dc017bdafee
florian <duceptflorian53@gmail.com>
2020-06-22 15:38:51 +0200

    bug corrigés

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function complete(obj) {
     let neo = document.createElement("p");
     result.appendChild(neo);
     neo.innerHTML = `Ville : ${obj[i].nom} <br> Population : ${obj[i].population} habitants`;
-    neo.style.color = "black";
     neo.classList.add("inert");
   }
 }
@@ -19,6 +18,10 @@ function get(postcode) {
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
       var response = JSON.parse(this.responseText);
       complete(response);
+    }
+    if (response.length == 0) {
+      document.querySelector(".main__result").innerHTML =
+        '<p class="inert">Ce code postale ne correspond à aucune ville</p>';
     }
   };
   request.open(
@@ -37,16 +40,3 @@ document.querySelector("#sub-btn").addEventListener("click", (e) => {
     result.innerHTML = "Entrer une addresse valide";
   }
 });
-
-/*var request = new XMLHttpRequest();
-request.onreadystatechange = function () {
-  if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-    var response = JSON.parse(this.responseText);
-    console.log(response[1].nom);
-  }
-};
-request.open(
-  "GET",
-  "https://geo.api.gouv.fr/communes?codePostal=77000&format=json"
-);
-request.send();*/
